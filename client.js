798fb4da2fcf15dff59bba4bf28728d306785105
Tyler Bonnell <tbonnell@uw.edu>
2016-06-17 21:32:15 -0500

    welcome to the game

--- client.js
+++ client.js
@@ -1,22 +1,35 @@
 window.onload = function() {
   var socket = io('http://localhost:3000');
 
+  // Player input
   var trackedKeys = {87:'w', 65:'a', 83:'s', 68:'d'};
   var keysDown = {};
-
   window.onkeydown = function(e) {
     if (!trackedKeys[e.keyCode]) return;
     if (!(e.keyCode in keysDown)) {
       keysDown[e.keyCode] = null;
       socket.emit('key down', e.keyCode);
     }
-  }
-
+  };
   window.onkeyup = function(e) {
     if (!trackedKeys[e.keyCode]) return;
     if (e.keyCode in keysDown) {
       delete keysDown[e.keyCode];
       socket.emit('key up', e.keyCode);
     }
-  }
+  };
+
+  // Messages from server
+  var players = {};
+  socket.on('player join', function(playerInfo) {
+    players[playerInfo.id] = playerInfo;
+  });
+  socket.on('player leave', function(playerInfo) {
+    delete playerInfo[playerInfo.id];
+  });
+
+
+  setInterval(function() {
+    document.getElementById("display").innerHTML = JSON.stringify(players);
+  }, 100);
 }
