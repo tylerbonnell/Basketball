b8bdad9a1556b2d86f7f1dcb86e696b91edaeea0
Tyler Bonnell <tbonnell@uw.edu>
2016-06-17 21:18:23 -0500

    associative array replacement

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-console.log("Server started");
+console.log("Server started!");
 
 app.get("/", function(req, res) {
   //res.sendFile(__dirname + '/index.html');
@@ -13,18 +13,22 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + '/game.html');
 });*/
 
-var games = {};
 var players = {};
 
 io.on('connection', function(socket) {
+  var info = players[socket.id] = {'keysDown':{}};
   //console.log(io.sockets.clients());
   //console.log(socket.id + " connected");
   socket.on('disconnect', function() {
     //console.log(this.id + " disconnected");
   });
 
-  socket.on('some message', function(msg) {
-    console.log(msg);
+  socket.on('key down', function(keyCode) {
+    info.keysDown[keyCode] = null;
+  });
+
+  socket.on('key up', function(keyCode) {
+    delete info.keysDown[keyCode];
   });
 });
 
--- client.js
+++ client.js
@@ -2,24 +2,21 @@ window.onload = function() {
   var socket = io('http://localhost:3000');
 
   var trackedKeys = {87:'w', 65:'a', 83:'s', 68:'d'};
-  var keysDown = [];
+  var keysDown = {};
 
   window.onkeydown = function(e) {
     if (!trackedKeys[e.keyCode]) return;
-    if (keysDown.indexOf(e.keyCode) < 0) {
-      keysDown.push(e.keyCode);
-      console.log("local log of keydown: " + e.keyCode);
-      socket.emit('some message', 'server log of keydown');
+    if (!(e.keyCode in keysDown)) {
+      keysDown[e.keyCode] = null;
+      socket.emit('key down', e.keyCode);
     }
   }
 
   window.onkeyup = function(e) {
     if (!trackedKeys[e.keyCode]) return;
-    var index = keysDown.indexOf(e.keyCode);
-    if (index >= 0) {
-      keysDown.splice(index, 1);
-      console.log("local log of keyup");
-      socket.emit('some message', 'server log of keyup');
+    if (e.keyCode in keysDown) {
+      delete keysDown[e.keyCode];
+      socket.emit('key up', e.keyCode);
     }
   }
 }
